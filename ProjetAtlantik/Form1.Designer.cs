bd9e03f999b3c168b5c7ae62fa60ff0fa4019295
unknown <de_araujo@220056slocal>
2025-03-11 17:06:40 +0100

    ok

---
+++ ProjetAtlantik/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ProjetAtlantik
+{
+    partial class PageAccueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            this.textBoxTest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.BackColor = System.Drawing.Color.GreenYellow;
+            this.buttonAjouter.Location = new System.Drawing.Point(476, 207);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(75, 23);
+            this.buttonAjouter.TabIndex = 0;
+            this.buttonAjouter.Text = "Ajouter";
+            this.buttonAjouter.UseVisualStyleBackColor = false;
+            this.buttonAjouter.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // textBoxTest
+            // 
+            this.textBoxTest.AccessibleName = "";
+            this.textBoxTest.Location = new System.Drawing.Point(464, 181);
+            this.textBoxTest.Name = "textBoxTest";
+            this.textBoxTest.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTest.TabIndex = 1;
+            // 
+            // PageAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(1255, 480);
+            this.Controls.Add(this.textBoxTest);
+            this.Controls.Add(this.buttonAjouter);
+            this.Name = "PageAccueil";
+            this.Text = "AjoutSecteur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAjouter;
+        private System.Windows.Forms.TextBox textBoxTest;
+    }
+}
+
---
+++ ProjetAtlantik/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetAtlantik
+{
+    public partial class PageAccueil : Form
+    {
+        private Button btnAjouter;
+        public PageAccueil()
+        {
+            InitializeComponent();
+            SuspendLayout();
+            Text = "Une première fenêtre";    // Le titre de la fenêtre
+            Size = new Size(200, 150);        // La taille initiale
+            MinimumSize = new Size(200, 150); // La taille minimale
+
+            // Le bouton "Ajouter"
+            btnAjouter = new Button();
+            btnAjouter.Text = "Fermer";
+            btnAjouter.AutoSize = true;             // Taille selon le contenu
+            btnAjouter.Location = new Point(50, 60);// Position x=50 y=60
+            btnAjouter.Click += new System.EventHandler(btnAjouter_Click);  /* Si Click sur « btnAjouter » on appelle la méthode « btnAjouter_Click ») */
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Secteur Ajouter");
+            Application.Exit();
+        }
+        public void buttonTest_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
